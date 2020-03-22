489e6a2662e71e2ec42f0afc2c5971e48b6815f4
Vignesh S <s.vnesh@gmail.com>
2020-03-23 01:19:48 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,10 +1,11 @@
 #Given a string and a character, find the index of the first occurence of the character.
-import re
-
 str1 = "alphabets"
-achar = 'a'
-result = str1.find(achar)
-print(result)
+achar = 'e'
 
-#result = re.search(achar, str1)
-#print(result.regs[0][0])
+def findindex(str1, achar):
+    lstr1 = list(str1)
+    for i in range(len(lstr1)):
+        if achar == str1[i]:
+            return i
+result = findindex(str1, achar)
+print(result)
