9cd294c4e1e55bddd382a9627531ac4adddfe1a0
Vignesh S <s.vnesh@gmail.com>
2020-03-24 23:48:07 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,4 +1,5 @@
 #P4: Given an array of integers, sort them in increasing order.
+#Bubble sort
 arr1 = [3,7,1,9,23,45,11,5]
 print('before: ',arr1)
 for j in range(1,len(arr1)):
@@ -6,3 +7,28 @@ for j in range(1,len(arr1)):
         if arr1[i] > arr1[i+1]:
             arr1[i], arr1[i+1] = arr1[i+1], arr1[i]
 print('after: ',arr1)
+
+#quick sort
+arr1 = [3,7,1,9,23,45,11,5]
+start = 0
+end = len(arr1)
+pIndex = None
+
+def qsort(arr1, start, end):
+    if (start < end):
+        pIndex = partition(arr1, start, end-1)
+        qsort(arr1, start, pIndex)
+        qsort(arr1, pIndex+1, end)
+
+def partition(arr1, start, end):
+    pviot = arr1[end]
+    pIndex = start
+    for i in range(start, end):
+        if arr1[i] <= pviot:
+            arr1[i],arr1[pIndex] = arr1[pIndex],arr1[i]
+            pIndex += 1
+    arr1[pIndex],arr1[end] = arr1[end],arr1[pIndex]
+    return pIndex
+
+qsort(arr1, start, end)
+print(arr1)
