bdcd05f6a18e157c8c1721bd2e0e52b0aed7af7a
Vignesh S <s.vnesh@gmail.com>
2020-03-26 00:40:43 +0530

    Update test_p2.py

--- p2/test/test_p2.py
+++ p2/test/test_p2.py
@@ -1,1 +1,12 @@
+import unittest
+from source.p2 import checkVowels
 
+class checkTheVowels(unittest.TestCase):
+    def test_vowel(self):
+        a = 'alphabets'
+        output = checkVowels(a)
+        actualResult = {'a': 2, 'e': 1, 'i':0, 'o':0, 'u':0}
+        self.assertDictEqual(output, actualResult)
+
+if __name__ == "__main__":
+    unittest.main()
