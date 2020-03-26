6ea94b7555917afd35564f91681f64f2cec4fec9
Vignesh S <s.vnesh@gmail.com>
2020-03-26 22:36:49 +0530

    Update p3.py

--- p3/source/p3.py
+++ p3/source/p3.py
@@ -1,1 +1,18 @@
+def findsubstring(fstring, sstring):
+    '''
+    #P3: Given a string and a substring, find whether the substring is found in the string.
+    '''
+    flstring = list(fstring)
+    slstring = list(sstring)
+    start = 0
 
+    if len(flstring) >= len(slstring):
+        for j in range(len(flstring)):
+            if slstring[0] == flstring[j]:
+                start = j
+                ss = fstring[start:start+len(slstring)]
+                if ss == sstring:
+                    return True
+        return False
+    else:
+        return 'Not valid substring'
