78a08f27b93fe87aa1bba8f241758e803d872352
Vignesh S <s.vnesh@gmail.com>
2020-03-26 22:38:07 +0530

    Update test_p3.py

--- p3/test/test_p3.py
+++ p3/test/test_p3.py
@@ -1,1 +1,20 @@
+import unittest
+from source.p3 import findsubstring
 
+class checkTheSubstring(unittest.TestCase):
+    def test_substring(self):
+        a = 'alphabets'
+        b = 'lph'
+        output = findsubstring(a, b)
+        actualResult = True
+        self.assertIs(output, actualResult, 'Failure')
+
+    def test_not_substring(self):
+        a = 'alpha'
+        b = 'alphabets'
+        output = findsubstring(a, b)
+        actualResult = 'Not valid substring'
+        self.assertEqual(output, actualResult, 'Failure')
+
+if __name__ == "__main__":
+    unittest.main()
