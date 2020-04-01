a8d2b9368501920d91c6f41f1cd307c3c7d4de56
Vignesh S <s.vnesh@gmail.com>
2020-04-01 22:28:30 +0530

    Add files via upload

--- p6/test/test_p6.py
+++ p6/test/test_p6.py
@@ -2,8 +2,12 @@ import unittest
 from source.p6 import reverseString
 
 class checkReversedString(unittest.TestCase):
-    def test_checkReverse(self):
+    def test_checkReverse1(self):
         self.assertEqual(reverseString('alphabets'), 'stebahpla', 'Failed')
+    def test_checkReverse2(self):
+        self.assertEqual(reverseString('ai'), 'ia', 'Failed')    
+    def test_checkReverse3(self):
+        self.assertEqual(reverseString('d'), 'd', 'Failed')
 
 if __name__ == '__main__':
     unittest.main()
