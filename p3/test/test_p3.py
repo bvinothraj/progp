ac9257b531e8d5091fb22f469a191dbe2f2582da
Vignesh S <s.vnesh@gmail.com>
2020-04-01 22:27:11 +0530

    Add files via upload

--- p3/test/test_p3.py
+++ p3/test/test_p3.py
@@ -2,19 +2,14 @@ import unittest
 from source.p3 import findsubstring
 
 class checkTheSubstring(unittest.TestCase):
-    def test_substring(self):
-        a = 'alphabets'
-        b = 'lph'
-        output = findsubstring(a, b)
-        actualResult = True
-        self.assertIs(output, actualResult, 'Failure')
-
-    def test_not_substring(self):
-        a = 'alpha'
-        b = 'alphabets'
-        output = findsubstring(a, b)
-        actualResult = 'Not valid substring'
-        self.assertEqual(output, actualResult, 'Failure')
+    def test_substring1(self):
+        self.assertIs(findsubstring('alphabets', 'lph'), True, 'Failure')
+    def test_substring2(self):
+        self.assertIs(findsubstring('checking', 'chck'), False, 'Failure')
+    def test_substring3(self):
+        self.assertIs(findsubstring('12345', '34'), True, 'Failure')
+    def test_not_substring4(self):
+        self.assertEqual(findsubstring('alpha', 'alphabets'), 'Not valid substring', 'Failure')
 
 if __name__ == "__main__":
     unittest.main()
