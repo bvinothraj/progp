8faab5516db046628ebb2a83a0047c325825e595
Vignesh S <s.vnesh@gmail.com>
2020-04-01 22:29:01 +0530

    Add files via upload

--- p7/test/test_p7.py
+++ p7/test/test_p7.py
@@ -2,8 +2,12 @@ import unittest
 from source.p7 import createMyList
 
 class checkMyList(unittest.TestCase):
-    def test_checkList(self):
+    def test_checkList1(self):
         self.assertEqual(createMyList([1,2,5,5,2,6,4,1]), [1,2,5,6,4], 'Failure')
+    def test_checkList2(self):
+        self.assertEqual(createMyList([1,1,1,1,1,2]), [1,2], 'Failure')    
+    def test_checkList3(self):
+        self.assertEqual(createMyList([7]), [7], 'Failure')
 
 if __name__ == '__main__':
     unittest.main()
