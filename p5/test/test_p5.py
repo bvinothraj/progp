5b087268cb0a3327f70eaf02022734e9cfc159f2
Vignesh S <s.vnesh@gmail.com>
2020-04-01 22:33:11 +0530

    Add files via upload

--- p5/test/test_p5.py
+++ p5/test/test_p5.py
@@ -8,7 +8,7 @@ class checkTheIndex(unittest.TestCase):
     def test_theIndex2(self):
         self.assertEqual(findindex('elephant', 'z'), -1, 'Failure')
     def test_theIndex3(self):
-        self.assertEqual(findindex('elephant', 'ph'), 'Not valid substring', 'Failure')
+        self.assertEqual(findindex('elephant', 'ze'), -1, 'Failure')
 
 if __name__ == "__main__":
     unittest.main()
